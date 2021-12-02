489c71a825b08bfc5ff27f40bc35dfe305aaa729
devashtar <72350748+devashtar@users.noreply.github.com>
2021-12-02 19:31:07 +0000

    Deploying to gh-pages from @ devashtar/mobile-app-next-js@cdb9dcc710d2d63c8145cb9ee6043fb9851640c1 🚀

--- _next/static/rM9ezQOhLF80ql94RJfCi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/357-c17c8584a0dafe21.js","static/chunks/pages/index-e1ba49aafded864d.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
